da464fa1ac00ff837348fe0384d5c30261ba3c9a
Tomas Koutsky <tomas@stepnivlk.net>
2016-06-13 14:41:17 +0200

    initial

--- lib/joinme2/configuration.rb
+++ lib/joinme2/configuration.rb
@@ -2,10 +2,10 @@ module Joinme2
   module Configuration
     AUTH_URL = 'https://secure.join.me/api/public/v1/auth/oauth2'
     URL = 'https://api.join.me/v1/'
-    API_KEY = 'da6rfunpz6fgyjhfxdm7pqdu'
-    REDIRECT_URI = 'https://developer.join.me/io-docs/oauth2callback'
+    API_KEY = ''
+    REDIRECT_URI = ''
     FORMAT = 'json'
-    CLIENT_ID = 'qxbg4y7epnybm3as38j6haf7'
+    CLIENT_ID = ''
     DEFAULT_SCOPES = 'user_info scheduler start_meeting'
 
     def self.extended(mod)
